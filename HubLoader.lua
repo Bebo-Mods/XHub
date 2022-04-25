9f6e35e0501ffaeb03195a34b102a4eaccdd8aae
Bebo-Mods <68604028+Bebo-Mods@users.noreply.github.com>
2022-04-25 22:49:28 +0200

    Update HubLoader.lua

--- HubLoader.lua
+++ HubLoader.lua
@@ -1,4 +1,6 @@
 -- Universal
-if game.placeId == nil then
+if game.placeId == (8540168650) then
+   loadstring(game:HttpGet("StandUpright", true))()
+else
    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/KittenMilkHub/main/Universal.lua", true))()
 end
