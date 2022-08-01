e5cbda30104ded78606a41eecfd5fdba53d0baa7
Bebo-Mods <68604028+Bebo-Mods@users.noreply.github.com>
2022-08-01 07:15:19 +0200

    Update HubLoader.lua

--- HubLoader.lua
+++ HubLoader.lua
@@ -1,7 +1,7 @@
-if game.PlaceId == (3851622790) then
-        loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/XHub/main/BreakInStoryLobby.lua", true))()
-elseif game.PlaceId == (4620170611) then
-    print("Adding Soon")
+if game.PlaceId == (3851622790) then -- Break In Story Lobby
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/XHub/main/BreakInStoryLobby.lua", true))()
+elseif game.PlaceId == (4620170611) then -- Break In Story Main
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/Bebo-Mods/XHub/main/BreakInStoryMain.lua", true))()
 else
     game.StarterGui:SetCore(
         "SendNotification",
